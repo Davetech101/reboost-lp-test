1e9d8cf8b64458ae8ba01203272b7f49f08659e5
Ogaranya <enomahog@gmail.com>
2022-09-26 20:58:39 +0100

    create nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,23 @@
+import { useState } from "react";
 import Hamburger from "../assets/Hamburger";
 import logo from "../assets/reboost-logo.7a83de35.png";
+import Nav from "./Nav";
 
 const Header = () => {
+  const [showNav, setShowNav] = useState(false)
   return (
-    <header>
+    <header style={{width: showNav ? "70%" : "100%" }}>
       <img src={logo} alt="logo" />
 
-      <div>
+      <div className={showNav && "move"}>
         <button className="contact-btn">Contact</button>
 
-        <button>
-          <Hamburger />
+        <button onClick={() => setShowNav(true)}>
+          <Hamburger/>
         </button>
       </div>
+
+      <Nav show={showNav} setShowNav={setShowNav}/>
     </header>
   );
 };
---
+++ src/components/Nav.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import Arrow from "../assets/Arrow";
+import Close from "../assets/Close";
+
+const Nav = ({ show, setShowNav }) => {
+  const [showServices, setShowServices] = useState(false);
+
+  const navItems = [
+    "Home",
+    "Services",
+    "About us",
+    "Work",
+    "Career",
+    "Sign up / Login",
+  ];
+
+  const services = [
+    "UI/UX Design",
+    " Web Development",
+    "Branding & Social Media",
+    "SEO & Copywriting",
+  ];
+
+  return (
+    <nav className={show && "show"}>
+      <button className="close" onClick={() => setShowNav(false)}>
+        <Close />
+      </button>
+
+      <ul>
+        {navItems.map((item) => (
+          <li key={item}>
+            <button
+              onClick={() =>
+                item === "Services" && setShowServices((prev) => !prev)
+              }
+            >
+              {item}{" "}
+              <span className={showServices ? "arrow rotate" : "arrow"}>{item === "Services" && <Arrow />}</span>
+            </button>
+
+            {item === "Services" && showServices && <ul className="services">
+              {services.map((service) => (
+                <li key={service}>{service}</li>
+              ))}
+            </ul>}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
